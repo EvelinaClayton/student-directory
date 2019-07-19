a59854314e37adcec9e28fed11f5a4b5b012ce68
Evelina <clayton.evelina@gmail.com>
2019-07-19 20:00:48 +0100

    Exercise 2: collecting student based on first letter

--- copy_directory.rb
+++ copy_directory.rb
@@ -16,28 +16,32 @@ def print_header
   puts "-------------------"
 end
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  puts "Collect student's name(s) beginning with this letter:"
+  first_letter = gets.chomp
+    students.each_with_index do |student, index|
+      if student[:name][0] == first_letter
+        puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      end
+    end
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
-
-students = input_students
+# def input_students
+#   puts "Please enter the names of the students"
+#   puts "To finish, just hit return twice"
+#   students = []
+#   name = gets.chomp
+#   while !name.empty? do
+#     students << {name: name, cohort: :november}
+#     puts "Now we have #{students.count} students"
+#     name = gets.chomp
+#   end
+#   students
+# end
+#
+# students = input_students
 print_header
 print(students)
 print_footer(students)
